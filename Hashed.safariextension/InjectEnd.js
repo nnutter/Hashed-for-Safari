f04ec2f927d29e8596a87d6d91cb792ac36e2fe4
Nathan Nutter <iam@nnutter.com>
2011-07-04 17:28:52 -0500

    specified BSD 3-clause as license

--- Hashed.safariextension/InjectEnd.js
+++ Hashed.safariextension/InjectEnd.js
@@ -1,32 +1,44 @@
+/*
+   Copyright (c) 2011, Nathaniel Nutter
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+   Neither the name of the Nathaniel Nutter nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   */
+
 if (window.top === window) {
-	function findPasswordFields() {
-		var passwordFields = new Array();
-		for (var f = 0; f < document.forms.length; f++) {
-			for (var e = 0; e < document.forms[f].elements.length; e++) {
-				if (document.forms[f].elements[e].type == 'password') {
-					passwordFields.push(document.forms[f].elements[e]);
-				}
-			}
-		}
-		return passwordFields;
-	}
-	
-	function receivePwdHash(msgEvent) {
-		if (msgEvent.name === "pwdHash") {
-			var passwordFields = findPasswordFields();
-			for (var e = 0; e < passwordFields.length; e++) {
-				passwordFields[e].value = msgEvent.message;
-				passwordFields[e].style.backgroundColor = "#ffffe0";
-			}
-		}
-		else if (msgEvent.name === "missingMasterPassword") {
-			alertMissingMasterPassword(msgEvent);
-		}
-	}
-	
-	function alertMissingMasterPassword(msgEvent) {
-		alert("You must set a Master Password in Hashed for Safari's extension settings.");
-	}
-	
-	safari.self.addEventListener("message", receivePwdHash, false);
+    function findPasswordFields() {
+        var passwordFields = new Array();
+        for (var f = 0; f < document.forms.length; f++) {
+            for (var e = 0; e < document.forms[f].elements.length; e++) {
+                if (document.forms[f].elements[e].type == 'password') {
+                    passwordFields.push(document.forms[f].elements[e]);
+                }
+            }
+        }
+        return passwordFields;
+    }
+
+    function receivePwdHash(msgEvent) {
+        if (msgEvent.name === "pwdHash") {
+            var passwordFields = findPasswordFields();
+            for (var e = 0; e < passwordFields.length; e++) {
+                passwordFields[e].value = msgEvent.message;
+                passwordFields[e].style.backgroundColor = "#ffffe0";
+            }
+        }
+        else if (msgEvent.name === "missingMasterPassword") {
+            alertMissingMasterPassword(msgEvent);
+        }
+    }
+
+    function alertMissingMasterPassword(msgEvent) {
+        alert("You must set a Master Password in Hashed for Safari's extension settings.");
+    }
+
+    safari.self.addEventListener("message", receivePwdHash, false);
 }
